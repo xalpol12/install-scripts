44d4b048536c163a77f804233ce7e7e5df5d39c5
Dawid Stawiszyński <xalpol12@wp.pl>
2024-02-17 13:11:02 +0100

    fix alacritty.sh script

--- apt/alacritty.sh
+++ apt/alacritty.sh
@@ -17,5 +17,10 @@ cargo build --release
 sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
 infocmp alacritty
 
+sudo cp target/release/alacritty /usr/local/bin
+
 # remove source code
-sudo rm -r alacritty
+cd .. && sudo rm -r alacritty
+
+# add alacritty to x-term alternatives
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/alacritty 50
