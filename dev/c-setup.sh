d2b58dcfe80470b6e50d231e7bed0dbc4a31729d
xalpol12 <xalpol12@gmail.com>
2024-03-29 23:06:00 +0100

    add c development setup

---
+++ dev/c-setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+sudo apt install gcc -y
+sudo apt install gdb -y
+sudo apt install valgrind -y
+sudo apt install gprof -y
+sudo apt install cmake -y
+sudo apt install pkg-config -y
+sudo apt install doxygen -y
